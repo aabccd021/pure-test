9c5637a9d16e385ec18df72ad1471c7e8822ecf2
aabccd021 <aabccd021@gmail.com>
2022-12-30 11:13:28 +0700

    fix: checkpoint

--- src/timeout.ts
+++ src/timeout.ts
@@ -1,29 +1,19 @@
-import { either, task } from 'fp-ts';
+import { task, taskEither } from 'fp-ts';
 import type { Either } from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import type { TaskEither } from 'fp-ts/TaskEither';
 
 import { test } from './core';
 
-type TimedOutError = { readonly type: 'timed out' };
+const timedOutError = { type: 'timed out' } as const;
 
 export const withTimeout =
-  <L, R>(millis: number): ((ma: TaskEither<L, R>) => TaskEither<L | TimedOutError, R>) =>
-  (ma) =>
-    task
-      .getRaceMonoid<Either<L | TimedOutError, R>>()
-      .concat(ma, task.delay(millis)(task.of(either.left({ type: 'timed out' as const }))));
-
-export const withTimeout2 =
   <A, L, R>(fab: (a: A) => TaskEither<L, R>) =>
   (p: A & { readonly timeout?: number }) =>
-    pipe(fab(p), withTimeout(p.timeout ?? 5000));
+    pipe(fab(p), (ma) =>
+      task
+        .getRaceMonoid<Either<L | typeof timedOutError, R>>()
+        .concat(ma, task.delay(p.timeout ?? 5000)(taskEither.left(timedOutError)))
+    );
 
-export const testWithTimeout = withTimeout2(test);
-
-export const a = testWithTimeout({
-  name: 'aab',
-  expect: async () => '',
-  toResult: '',
-  timeout: 10,
-});
+export const testWithTimeout = withTimeout(test);
