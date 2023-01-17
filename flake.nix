285fecec04ca16570b4060bded642402de4528ee
aabccd021 <aabccd021@gmail.com>
2023-01-17 16:50:34 +0700

    build: eslint

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
     devShell.x86_64-linux = mkShellNoCC {
       buildInputs = [
-        nodejs-16_x
+        nodejs-18_x
         nodePackages.pnpm
       ];
       shellHook = ''
