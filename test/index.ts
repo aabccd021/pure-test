6b0f2b5f96c62bf7c9db71b3dd083dbef2e04916
aabccd021 <aabccd021@gmail.com>
2023-01-19 08:44:12 +0700

    refactor: name

--- test/index.ts
+++ test/index.ts
@@ -4,11 +4,11 @@ import { pipe } from 'fp-ts/function';
 import * as src from '../src';
 import * as srcNode from '../src/node';
 import * as exit from './exitF';
-import * as log from './log';
+import * as logErrorDetails from './log';
 import * as primitive from './primitive';
 import * as timeout from './timeout';
 
-const tests = [log.tests, exit.tests, timeout.tests, primitive.tests];
+const tests = [logErrorDetails.tests, exit.tests, timeout.tests, primitive.tests];
 
 export const main = pipe(
   tests,
