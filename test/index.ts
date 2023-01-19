33ba41670b3bf58460042ef02081c54ee7257261
aabccd021 <aabccd021@gmail.com>
2023-01-19 08:45:11 +0700

    fix: rename

--- test/index.ts
+++ test/index.ts
@@ -4,7 +4,7 @@ import { pipe } from 'fp-ts/function';
 import * as src from '../src';
 import * as srcNode from '../src/node';
 import * as exit from './exitF';
-import * as logErrorDetails from './log';
+import * as logErrorDetails from './logErrorDetails';
 import * as primitive from './primitive';
 import * as timeout from './timeout';
 
