a31019785648d3f1ab80a87781be93abd021e822
aabccd021 <aabccd021@gmail.com>
2023-01-21 19:15:24 +0700

    fix: eslint-plugin-node

--- src/node.ts
+++ src/node.ts
@@ -1,1 +1,5 @@
-export { exit } from './exit';
+import process from 'process';
+
+import { exitF } from './exitF';
+
+export const exit = exitF({ process: { exit: (exitCode) => () => process.exit(exitCode) } });
