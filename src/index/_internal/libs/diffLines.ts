845da4c63fd0cd826fecdf1df5085072c1c75003
aabccd021 <aabccd021@gmail.com>
2023-01-25 06:04:40 +0700

    refactor: diffLines

--- src/index/_internal/libs/diffLines.ts
+++ src/index/_internal/libs/diffLines.ts
@@ -3,33 +3,35 @@ import { readonlyArray, string } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
 import { match } from 'ts-pattern';
 
-import type { DiffLines } from '../../type';
+import type { Change, DiffLines } from '../../type';
 
-const removeLastNewLine = (str: string) =>
-  string.endsWith('\n')(str)
-    ? pipe(
-        str,
-        string.split(''),
-        readonlyArray.dropRight(1),
-        readonlyArray.intercalate(string.Monoid)('')
-      )
-    : str;
+const removeLastChar = (str: string) =>
+  pipe(
+    str,
+    string.split(''),
+    readonlyArray.dropRight(1),
+    readonlyArray.intercalate(string.Monoid)('')
+  );
+
+const removeLastNewLine = (str: string) => (string.endsWith('\n')(str) ? removeLastChar(str) : str);
+
+const adaptChange = (change: diff.Change): Change =>
+  match(change)
+    .with({ added: true }, ({ value }) => ({ type: '+' as const, value }))
+    .with({ removed: true }, ({ value }) => ({ type: '-' as const, value }))
+    .otherwise(({ value }) => ({ type: '0' as const, value }));
+
+const mapChangeForEachLines = (change: Change): readonly Change[] =>
+  pipe(
+    change.value,
+    removeLastNewLine,
+    string.split('\n'),
+    readonlyArray.map((value) => ({ type: change.type, value }))
+  );
 
 export const diffLines: DiffLines = ({ expected, received }) =>
   pipe(
     diff.diffLines(expected, received),
-    readonlyArray.map((change) =>
-      match(change)
-        .with({ added: true }, ({ value }) => ({ type: '+' as const, value }))
-        .with({ removed: true }, ({ value }) => ({ type: '-' as const, value }))
-        .otherwise(({ value }) => ({ type: '0' as const, value }))
-    ),
-    readonlyArray.chain((change) =>
-      pipe(
-        change.value,
-        removeLastNewLine,
-        string.split('\n'),
-        readonlyArray.map((value) => ({ type: change.type, value }))
-      )
-    )
+    readonlyArray.map(adaptChange),
+    readonlyArray.chain(mapChangeForEachLines)
   );
