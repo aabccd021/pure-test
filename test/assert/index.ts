e1c84ac728da8a811c983ddd8c517169a5136aae
aabccd021 <aabccd021@gmail.com>
2023-01-25 19:33:49 +0700

    fix: stringInLines

--- test/assert/index.ts
+++ test/assert/index.ts
@@ -2,5 +2,6 @@ import { scope } from '@src';
 
 import * as either from './either';
 import * as equal from './equal';
+import * as stringInLinesEqual from './stringInLinesEqual.ts';
 
-export const tests = scope({ equal, either });
+export const tests = scope({ equal, either, stringInLinesEqual });
